bb8fdf96bde9bb530b3b3b0ad9f7c5c1ce4618c7
onContentStop <kylecoffey1999@gmail.com>
2021-04-11 15:16:30 -0500

    Initial commit

---
+++ monky/src/token.rs
@@ -0,0 +1,58 @@
+use std::fmt::Display;
+
+/// The kind of a token.
+pub enum TokenKind {
+    Illegal,
+    Eof,
+
+    // Identifiers + literals
+    Ident,
+    Int,
+
+    // Operators
+    Assign,
+    Plus,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+
+    ParenL,
+    ParenR,
+    BraceL,
+    BraceR,
+
+    // Keywords
+    Function,
+    Let,
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TokenKind::Illegal => "ILLEGAL",
+                TokenKind::Eof => "EOF",
+                TokenKind::Ident => "IDENT",
+                TokenKind::Int => "INT",
+                TokenKind::Assign => "=",
+                TokenKind::Plus => "+",
+                TokenKind::Comma => ",",
+                TokenKind::Semicolon => ";",
+                TokenKind::ParenL => "(",
+                TokenKind::ParenR => ")",
+                TokenKind::BraceL => "{",
+                TokenKind::BraceR => "}",
+                TokenKind::Function => "FUNCTION",
+                TokenKind::Let => "LET",
+            }
+        )
+    }
+}
+
+pub struct Token {
+    pub kind: TokenKind,
+    pub literal: String,
+}
