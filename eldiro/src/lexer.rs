d0e5e1de98af37f2a34b7a29d7e364a3affeb036
onContentStop <kylecoffey1999@gmail.com>
2021-04-16 21:46:45 -0500

    Eldiro lexer

--- eldiro/src/lexer.rs
+++ eldiro/src/lexer.rs
@@ -6,16 +6,110 @@ enum SyntaxKind {
     Whitespace,
     #[error]
     Error,
+
+    #[token("fn")]
+    KwFn,
+    #[token("let")]
+    KwLet,
+
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
+    Ident,
+    #[regex(r"[0-9]+")]
+    Num,
+
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Star,
+    #[token("/")]
+    Slash,
+    #[token("=")]
+    Equals,
+    #[token("{")]
+    BraceL,
+    #[token("}")]
+    BraceR,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn check_single(input: &str, expected_kind: SyntaxKind) {
+        let mut lexer = SyntaxKind::lexer(input);
+        assert_eq!(lexer.next(), Some(expected_kind));
+        assert_eq!(lexer.slice(), input);
+    }
+
     #[test]
     fn lex_spaces() {
-        let mut lexer = SyntaxKind::lexer("  ");
+        check_single("  ", SyntaxKind::Whitespace);
+    }
 
-        assert_eq!(lexer.next(), Some(SyntaxKind::Whitespace));
-        assert_eq!(lexer.slice(), "  ");
+    #[test]
+    fn lex_fn_keyword() {
+        check_single("fn", SyntaxKind::KwFn);
+    }
+
+    #[test]
+    fn lex_let_keyword() {
+        check_single("let", SyntaxKind::KwLet);
+    }
+
+    #[test]
+    fn lex_alphabetic_identifier() {
+        check_single("abcdefg", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_alphanumeric_identifier() {
+        check_single("abc123", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_mixed_case_identifier() {
+        check_single("abABcD3", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_number() {
+        check_single("489320", SyntaxKind::Num);
+    }
+
+    #[test]
+    fn lex_plus() {
+        check_single("+", SyntaxKind::Plus);
+    }
+
+    #[test]
+    fn lex_minus() {
+        check_single("-", SyntaxKind::Minus);
+    }
+
+    #[test]
+    fn lex_star() {
+        check_single("*", SyntaxKind::Star);
+    }
+
+    #[test]
+    fn lex_slash() {
+        check_single("/", SyntaxKind::Slash);
+    }
+
+    #[test]
+    fn lex_equals() {
+        check_single("=", SyntaxKind::Equals);
+    }
+
+    #[test]
+    fn lex_left_brace() {
+        check_single("{", SyntaxKind::BraceL);
+    }
+
+    #[test]
+    fn lex_right_brace() {
+        check_single("}", SyntaxKind::BraceR);
     }
 }
