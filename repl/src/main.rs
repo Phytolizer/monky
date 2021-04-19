0be69a9ce28dbc89c47d18e0ef58325794f43804
onContentStop <kylecoffey1999@gmail.com>
2021-04-19 13:03:06 -0500

    RPPL improvements

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -2,11 +2,15 @@ use dialoguer::console::style;
 use dialoguer::console::Style;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Input;
-use log::warn;
 use monky::parser::Parser;
+use slog::warn;
+use slog::Drain;
 
 fn main() {
-    log4rs::init_file("monky-log.yml", Default::default()).unwrap();
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = slog::Logger::root(drain, slog::o!());
     let theme = ColorfulTheme {
         prompt_prefix: style("monky".into()).yellow(),
         prompt_style: Style::new().dim(),
@@ -28,7 +32,7 @@ fn main() {
         let mut parser = Parser::new(&text);
         let program = parser.parse_program();
         for error in parser.errors() {
-            warn!("{}", error);
+            warn!(log, "{}", error);
         }
 
         println!("{}", program);
